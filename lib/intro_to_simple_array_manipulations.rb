822be9901acd68561f165cab660f8bbaa7816912
Gabriel Castro <gabriel.castro1028@gmail.com>
2020-05-18 17:51:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
-def using_concat
+def using_concat(my_favorite_things, more_favs)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
   all_my_favs.using_concat(my_favorite_things,more_favs)
