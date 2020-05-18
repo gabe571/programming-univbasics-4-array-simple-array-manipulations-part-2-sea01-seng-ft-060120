fc5c9cbe5c4a8771a37f66280aa394e5b7b885c1
Gabriel Castro <gabriel.castro1028@gmail.com>
2020-05-18 17:52:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
-def using_concat(my_favorite_things, more_favs)
+def using_concat
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
   all_my_favs.concat(my_favorite_things,more_favs)
