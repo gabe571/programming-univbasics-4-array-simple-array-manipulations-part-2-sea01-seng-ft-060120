8675334ce158a18f536fc1dca4fae2eac862485f
Gabriel Castro <gabriel.castro1028@gmail.com>
2020-05-18 17:50:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,17 @@
+def using_concat
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  all_my_favs = using_concat(my_favorite_things,more_favs)
+end
 
+
+
+describe "using_concat" do 
+  before(:each) do 
+    @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+    @all_my_favs = using_concat(@my_favorite_things, more_favs)
+  end
 
 
 def using_insert(array, element)
